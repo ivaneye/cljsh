b50cca0f33c3bf47aec3e7b4eee3bb4bdb2d131f
ivanpig <ivan.pig@foxmail.com>
2014-06-22 23:30:52 +0800

    change ls

--- src/cljsh/ls.clj
+++ src/cljsh/ls.clj
@@ -1,23 +1,10 @@
 (ns cljsh.ls
   (:require [clojure.java.io :as io]))
 
-
-
-(def f (io/file "."))
-
-(def fns (vec (.list f)))
-
-(.isDirectory f)
-
 (defn ls
   ([& param]
    (if (>= (count param) 1)
      (let [path (last param)]
        (-> path io/file .list vec))))
   ([] (ls ".")))
-
-
-(ls)
-(ls ".")
-(ls "/")
 
